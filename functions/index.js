1b5ad6b06803f7624f52feb07109ce3f0b5762bc
Jason Huang <jasonhuang20035@gmail.com>
2021-05-01 01:30:33 -0400

    fix: migrate off of pptr due to memory constriants

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require("firebase-functions");
-const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
 const fetch = require("node-fetch");
 const admin = require("firebase-admin");
 
@@ -14,63 +14,47 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 const scrape = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  try {
-    const sitesRef = admin.firestore().collection("sites");
-    await admin.firestore().runTransaction(async (t) => {
-      const sitesSnapshot = await t.get(sitesRef);
-      sitesSnapshot.forEach(async (doc) => {
-        // navigate to page
-        await page.goto(doc.data().url, {
-          waitUntil: "networkidle0",
-        });
-
-        // get HTML element from DOM
-        const innerHTML = await page.$eval(
-          doc.data().selector,
-          (el) => el.innerHTML
-        );
-        // run diff
-        if (innerHTML !== doc.data().prevInnerHTML) {
-          // there is a difference
-          console.log(`diff detected for ${doc.id}`);
-          const discordURL = functions.config().discord.url;
-          const webhookParams = {
-            content: `${
-              doc.data().name
-            }: Change Detected! <@&837857458848137300> \n${doc.data().url}`,
-            allowed_mentions: {
-              parse: ["roles"],
-            },
-          };
-          await fetch(discordURL, {
-            method: "post",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(webhookParams),
-          });
-        }
-        // merge innerHtml into Firebase
-        t.update(doc.ref, {
-          prevInnerHTML: innerHTML,
-        });
+  const sitesRef = admin.firestore().collection("sites");
+  const sitesSnapshot = await sitesRef.get();
+  // loop through the sites
+  sitesSnapshot.forEach(async (doc) => {
+    // download page
+    const $ = cheerio.load(await (await fetch(doc.data().url)).text());
+    // get HTML element from DOM
+    const innerHTML = $(doc.data().selector).html();
+    // run diff
+    if (innerHTML !== doc.data().prevInnerHTML) {
+      // there is a difference
+      console.log(`diff detected for ${doc.id}`);
+      const discordURL = functions.config().discord.url;
+      const webhookParams = {
+        content: `${
+          doc.data().name
+        }: Change Detected! <@&837857458848137300> \n${doc.data().url}`,
+        allowed_mentions: {
+          parse: ["roles"],
+        },
+      };
+      await fetch(discordURL, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(webhookParams),
       });
+    }
+    // merge innerHtml into Firebase
+    await doc.ref.update({
+      prevInnerHTML: innerHTML,
     });
-  } catch (err) {
-    // close the browser in the case of an error
-    await browser.close();
-    throw err;
-  }
-
-  // close the browser
-  await browser.close();
+  });
 };
 
 if (functions.config().isDebug) {
-  exports.scrape = functions.https.onRequest(scrape);
+  exports.scrape = functions.https.onRequest((req, res) => {
+    scrape();
+    res.send();
+  });
 } else {
   exports.scrape = functions.pubsub.schedule("every 2 minutes").onRun(scrape);
 }
