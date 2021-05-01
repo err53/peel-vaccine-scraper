193138ce83219e8537d451b92510ee859964df9c
Jason Huang <jasonhuang20035@gmail.com>
2021-05-01 00:48:01 -0400

    style: update eslintrc formatting

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,11 @@
 module.exports = {
   root: true,
   parserOptions: {
-    "ecmaVersion": 8,
+    ecmaVersion: 8,
   },
   env: {
     es6: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
 };
